6cef42380699493ed404ac845f0cd85eafff179d
meebox <meebox@gmail.com>
2021-09-14 18:25:16 +0800

    搭配 Blynk 改版為 Blynk IoT 修改 Lab14 及 程式庫
    On branch BlynkIoT
    Changes to be committed:
    modified:   "FM617A 用 Python 學物聯網\程式庫\BlynkLib.py"
    new file:   "FM617A 用 Python 學物聯網\程式庫\BlynkTimer.py"
    modified:   "FM617A 用 Python 學物聯網\範例檔\Lab14.py"

--- FM617A 用 Python 學物聯網/範例檔/Lab14.py
+++ FM617A 用 Python 學物聯網/範例檔/Lab14.py
@@ -4,36 +4,34 @@
 
 from machine import Pin
 import dht, BlynkLib, network              # 匯入 Blynk 模組
+from BlynkTimer import BlynkTimer
 
 sta_if = network.WLAN(network.STA_IF)      # 取得無線網路介面
 sta_if.active(True)                        # 取用無線網路
-sta_if.connect('無線網路名稱', '密碼')      # 連結無線網路
+sta_if.connect('無線網路名稱', '無線網路密碼') # 連結無線網路
 while not sta_if.isconnected():            # 等待連上無線網路
     pass               
 print("Wifi已連上")                         # 顯示連上網路的訊息
 
-token = '這裡要填入剛剛收到的權杖'           # Blynk 寄給你的權杖
+token = 'Blynk 裝置的認證權杖'               # 裝置的認證權杖
 blynk = BlynkLib.Blynk(token)              # 取得 Blynk 物件
 
 sensor = dht.DHT11(Pin(0))                 # 使用 D3 腳位取得溫溼度物件
 relay = Pin(14, Pin.OUT, value = 0)        # 使用 D5 腳位控制繼電器
 
-def v1_handler():              # 提供溫度到 V1 虛擬腳位的函式
-    sensor.measure()
-    blynk.virtual_write(1, sensor.temperature())
-
-def v2_handler():              # 提供濕度到 V2 虛擬腳位的函式
-    # 使用剛剛讀取的感測值, 避免讀取時間間隔太近
-    blynk.virtual_write(2, sensor.humidity())
-
-def v3_handler(value):         # 從 V3 虛擬腳位讀取手機按鈕狀態的函式
+def v3_handler(value):           # 從 V3 虛擬腳位讀取手機按鈕狀態的函式
     relay.value(int(value[0]))
 
-blynk.on("readV1", v1_handler) # 註冊由 v1_handler 處理 V1 虛擬腳位
-blynk.on("readV2", v2_handler) # 註冊由 v2_handler 處理 V2 虛擬腳位
+def temp_huni_handler():         # 提供溫/濕度到 V1/V2 虛擬腳位的函式
+    sensor.measure()
+    blynk.virtual_write(1, sensor.temperature())
+    blynk.virtual_write(2, sensor.humidity())
+
+timer = BlynkTimer()                       # 建立計時器管理物件
+timer.set_interval(3, temp_huni_handler)   # 建立週期性計時器, 定時傳送溫濕度
+
 blynk.on("V3", v3_handler)     # 註冊由 v3_handler 處理 V3 虛擬腳位
 
 while True:
     blynk.run()                # 持續檢查是否有收到 Blynk 送來的指令
-    
-    
+    timer.run()                # 持續檢查是否觸發計時器
